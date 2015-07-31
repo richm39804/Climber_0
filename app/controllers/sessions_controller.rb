0b2349d692965f34fe4f7e5a78443db893970859
richm39804git config --global user.email richmontero25@gmail.comgit config --global user.name richm39804 <richmontero25@gmail.com>
2015-07-31 11:05:20 -0700

    repo commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ def new
  def create
   user = User.find_by_email(params[:email])
   if user && user.authenticate(params[:password])
-   redirect_to root_path, notice: "logged in!"
+   redirect_to new_coupon_path, notice: "logged in!"
   else
    flash.now.alert = "invalid login credentials"
    render "new"
