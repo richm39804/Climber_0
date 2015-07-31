6bf1b5a505af1f4d0fbd4cb57ce218b0177a3d78
richm39804git config --global user.email richmontero25@gmail.comgit config --global user.name richm39804 <richmontero25@gmail.com>
2015-07-31 09:28:48 -0700

    initialized repo

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,7 +4,19 @@ class CouponsController < ApplicationController
 
   def new
   end
+  def edit
+  end
 
   def update
   end
+  def create
+    @coupon = User.new(user_params)
+    if @coupon.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
 end
+
--- db/migrate/20150731014530_create_categories.rb
+++ db/migrate/20150731014530_create_categories.rb
@@ -3,6 +3,9 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.string :name
       t.string :img_url
+      t.string :title
+      t.string :location
+      t;string :price
       t.integer :users_id
       t.timestamps null: false
     end
